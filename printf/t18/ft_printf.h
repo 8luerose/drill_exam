be904754d306003428b705f6bd0d5a4dc4d9a770
8luerose <8luerose@naver.com>
2023-12-12 12:28:45 +0900

    [REFACTOR] gnl_t1

---
+++ printf/t18/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 12:06:49 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/12/12 12:08:51 by taehkwon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+int ft_printf(const char *format, ...);
+void check(char c, va_list *ap, int *bytes);
+void ft_putchar(char c, int *bytes);
+void ft_putstr(char *str, int *bytes);
+void ft_print_number(long n, char *base_arr, int len, int *bytes);
+
+#endif
