ad16c25dbea29813f9f3b45dcf4ee3e29f19345a
8luerose <8luerose@naver.com>
2023-12-19 04:18:20 +0900

    [REFACTOR] gnl t13, printf t22

---
+++ printf/test/t22/main.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+int main(void)
+{
+	char *str;
+	int i;
+	unsigned int j;
+
+	str = "hello world";
+	i = 42;
+	j = 16;
+
+	ft_printf("%s %d %x\n", str, i, j);
+	return (0);
+}
